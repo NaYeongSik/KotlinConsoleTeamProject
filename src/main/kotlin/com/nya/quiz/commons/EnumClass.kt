26253fcaac677cdd94e17abefa48438afcddf9e5
NaYoungSik <nadudtlr@naver.com>
2025-05-26 17:59:57 +0900

    Main Process 구조 추가  - Main.kt
    동작 처리 관련 Object 추가
     - StateManager.kt (상태 처리)
     - ConsoleManager.kt (콘솔 입력 처리)
    
    Enum 클래스 수정 및 ConstantFile 추가

--- src/main/kotlin/com/nya/quiz/commons/EnumClass.kt
+++ src/main/kotlin/com/nya/quiz/commons/EnumClass.kt
@@ -17,3 +17,30 @@ enum class LoginState(var state:Int){
     LOGGED_IN(1),
     LOGIN_FAILED(2)
 }
+
+enum class ViewState(val state:Int){
+    START_VIEW(1),
+    MAIN_VIEW(2),
+    END_VIEW(3) // 프로그램 종료 조건으로 사용하기위해 추가
+}
+
+enum class StartViewState(var state:Int) {
+    LOG_IN(1),
+    SIGN_UP(2),
+    HELP(3),
+    EXIT(4);
+
+    companion object { // 사용자가 입력한 값을 Int로 변환하여 대조해서 처리하려했으나 직접적인 대조가 안되어 추가했습니다.
+        fun fromInt(code: Int): StartViewState? = entries.find { it.state == code }
+    }
+}
+
+enum class MainMenuState(var state:Int){
+    START_QUIZ(1),
+    RETRY_INCORRECT_WORD(2),
+    INCORRECT_NOTE(3),
+    RANK(4),
+    LOG_OUT(5),
+    DELETE_ACCOUNT(6),
+    EXIT(7)
+}
