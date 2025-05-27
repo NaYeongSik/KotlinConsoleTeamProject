21a8c3c456f19a5c9a2406efc0af53dd1df0364e
a <yangjk9203@naver.com>
2025-05-27 17:53:10 +0900

    a ㅁㅁㅁ

--- src/main/kotlin/com/nya/quiz/viewmodels/startVIewModels/HelpViewModel.kt
+++ src/main/kotlin/com/nya/quiz/viewmodels/startVIewModels/HelpViewModel.kt
@@ -1,2 +1,11 @@
 package com.nya.quiz.viewmodels.startVIewModels
 
+import com.nya.quiz.views.startview.Help
+
+class HelpViewModel {
+    private val help = Help()
+    fun help(){
+        help.announce()
+        help.undo()
+    }
+}
--- src/main/kotlin/com/nya/quiz/viewmodels/startVIewModels/LoginViewModel.kt
+++ src/main/kotlin/com/nya/quiz/viewmodels/startVIewModels/LoginViewModel.kt
@@ -1,2 +1,28 @@
 package com.nya.quiz.viewmodels.startVIewModels
 
+import com.nya.quiz.commons.ConsoleManager
+import com.nya.quiz.commons.StateManager
+import com.nya.quiz.models.User
+import com.nya.quiz.viewmodels.mainViewModels.EntryMainViewModels
+
+class LoginViewModel {
+
+    fun login() {
+        val entryMainViewModels = EntryMainViewModels()
+        // 1. 아이디 입력 및 검사
+        while (true) {
+            print("아이디를 입력하세요. : ")
+            val inputUsername = ConsoleManager.consoleLine()
+            print("비밀번호를 입력하세요. : ")
+            val inputPassword = ConsoleManager.consoleLine()
+            if (User.loginUser(inputUsername, inputPassword)) {
+                println("로그인 되었습니다.")
+                StateManager.updateState("1")
+                    entryMainViewModels.selectMainMenu(inputUsername)
+                    break
+            } else {
+                println("입력정보가 틀렸습니다. 다시 입력하세요.")
+            }
+        }
+    }
+}
