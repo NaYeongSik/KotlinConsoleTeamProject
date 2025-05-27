b496a045eeadda5e5773f1a329b779f7b6db0fb5
a <yangjk9203@naver.com>
2025-05-27 21:04:14 +0900

    deleteAccount 추가

--- src/main/kotlin/com/nya/quiz/viewmodels/mainViewModels/DeleteAccountViewModel.kt
+++ src/main/kotlin/com/nya/quiz/viewmodels/mainViewModels/DeleteAccountViewModel.kt
@@ -1,3 +1,36 @@
 package com.nya.quiz.viewmodels.mainViewModels
 
-class DeleteAccountViewModel
+import com.nya.quiz.commons.ConsoleManager
+import com.nya.quiz.views.mainview.DeleteAccount
+import com.nya.quiz.models.User.accountFile
+import com.nya.quiz.views.startview.EntryMain
+
+
+class DeleteAccountViewModel(connectingId: String) {
+    val deleteAccount = DeleteAccount()
+    val entryMain = EntryMain()
+    val currentId = connectingId
+
+    fun deleteAccount(): Boolean {
+        deleteAccount.recheckMessage()
+        while (true) {
+            val input = ConsoleManager.consoleLine()
+            if (input == "1") {
+                accountFile.useLines { lines ->
+                    lines.forEach { line ->
+                        val parts = line.split(',')[0]
+                        if (parts == currentId) {
+                            accountFile.writeText(lines.joinToString("\n"))
+                        }
+                    }
+                }
+                deleteAccount.completeMessage()
+                return true
+            }
+            if (input == "2") {
+                return false
+            }
+            entryMain.errorMessage()
+        }
+    }
+}
