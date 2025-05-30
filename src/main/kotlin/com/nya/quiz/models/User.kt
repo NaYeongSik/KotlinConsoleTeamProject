6fb5ff45ea60b9ee489a2076377b48653363c55f
PEANUTBUTTER1001 <yangjk9203@naver.com>
2025-05-30 16:34:07 +0900

    계정관리 삭제 추가 구현

--- src/main/kotlin/com/nya/quiz/models/User.kt
+++ src/main/kotlin/com/nya/quiz/models/User.kt
@@ -1,11 +1,14 @@
 package com.nya.quiz.models
 
+import com.nya.quiz.commons.QuizStat
 import com.nya.quiz.commons.StateManager
 import java.io.File
 
 object User {
     private const val ACCOUNT_FILE_PATH = "src/main/resources/account.txt"
+    private const val ACCOUNT_STAT_FILE_PATH = "src/main/resources/userStatInfo.txt"
     val accountFile = File(ACCOUNT_FILE_PATH)
+    val accountStatFile = File(ACCOUNT_STAT_FILE_PATH)
     var accountId = ""
 
     fun setId(input: String) {
@@ -18,10 +21,12 @@ object User {
         accountId = ""
     }
 
-    // 새 계정 저장
+    // 새 계정 생성
     fun saveAccount(username: String, password: String) {
-        val accountData = "$username,$password\n"
-        accountFile.appendText(accountData)
+        val accountData = "$username,$password"
+        val accountStatData = QuizStat().copy(userId = username)
+        accountFile.appendText(accountData+"\n")
+        accountStatFile.appendText(accountStatData.toString()+"\n")
     }
 
     // 계정 검증
@@ -40,3 +45,4 @@ object User {
         }
     }
 }
+
--- src/main/kotlin/com/nya/quiz/viewmodels/startVIewModels/SignUpViewModel.kt
+++ src/main/kotlin/com/nya/quiz/viewmodels/startVIewModels/SignUpViewModel.kt
@@ -16,7 +16,7 @@ object SignUpViewModel {
         while (true) {
             print("사용할 아이디를 입력하세요 (4~12자, 영문/숫자): ")
             val inputUsername = ConsoleManager.consoleLine()
-            val (isValid, message) =isValidUsername(inputUsername)
+            val (isValid, message) = isValidUsername(inputUsername)
             println(message)
             if (isValid) {
                 username = inputUsername
@@ -40,13 +40,12 @@ object SignUpViewModel {
             val confirmPassword = readLine() ?: ""
             if (password == confirmPassword) {
                 println("비밀번호가 확인되었습니다.")
+                User.saveAccount(username, password)
+                println("회원가입이 완료되었습니다. 시작 메뉴로 돌아갑니다.")
                 break
             } else {
                 println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.")
             }
         }
-
-        User.saveAccount(username, password)
-        println("회원가입이 완료되었습니다. 시작 메뉴로 돌아갑니다.")
     }
 }
