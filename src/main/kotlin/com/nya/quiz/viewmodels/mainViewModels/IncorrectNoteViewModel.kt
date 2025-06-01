014c8921d4be28a03d3fa07a51e6b055e0b7dbb0
universeYong <boyd0509@gmail.com>
2025-06-01 15:12:20 +0900

    도움말추가 및 Validator이름 변경

--- src/main/kotlin/com/nya/quiz/viewmodels/mainViewModels/IncorrectNoteViewModel.kt
+++ src/main/kotlin/com/nya/quiz/viewmodels/mainViewModels/IncorrectNoteViewModel.kt
@@ -11,10 +11,25 @@ class IncorrectNoteViewModel{
         var quizList = myProfile.incorrectQuiz
         var stringBuilder = StringBuilder()
 
-        for (quiz:String in quizList){
-            stringBuilder.append(quiz)
-            stringBuilder.append("\n")
+        if (quizList.isEmpty()) {
+            stringBuilder.append("저장된 오답 문제가 없습니다.\n")
+        } else {
+
+            val lastQuizListStr = quizList.last()
+            val regex = Regex("""QuizWord\(word=([^,]+), meanings=\[([^\]]*)\]\)""")
+            val formattedList = regex.findAll(lastQuizListStr).map { match ->
+                val word = match.groupValues[1].trim()
+                val meanings = match.groupValues[2].split(',').map { it.trim() }.joinToString(", ")
+                "$word : $meanings"
+            }.toList()
+
+            // 10개씩 한 줄에 출력
+            formattedList.chunked(10).forEach { lineList ->
+                stringBuilder.append(lineList.joinToString("    "))
+                stringBuilder.append("\n")
+            }
         }
+
         stringBuilder.append("\n진행한 문제 수 : ${myProfile.correctCount + myProfile.incorrectCount}\n정답률: ${myProfile.correctRate}%")
 
         return stringBuilder.toString()
